dacab06444515b7dafd631a8e614144406d52aa0
Anjali <aj2966@gmail.com>
2018-07-18 00:23:33 +0530

    final changes done

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
-from blogposts import views
+import blogposts.views
+import sitepages.views
 #to get images
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
+    url(r'^$', blogposts.views.home, name="home"),
     #post_id is just a variable which will later be used in views.py to obtain post.id
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details, name="blogpost_details"),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', blogposts.views.post_details, name="blogpost_details"),
+    url(r'^about/', sitepages.views.about, name="about")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
